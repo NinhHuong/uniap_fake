406d1cce8073df8f4bcb00a34c9073a3e70e1e09
TuanNA <bonz@Bonzs-MacBook-Pro.local>
2017-03-21 04:30:55 +0700

    Login data firebase

--- app/src/main/java/com/example/bonz/uniap_fake/activity/LoginActivity.java
+++ app/src/main/java/com/example/bonz/uniap_fake/activity/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.bonz.uniap_fake.activity;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,19 +12,29 @@ import android.widget.EditText;
 import com.example.bonz.uniap_fake.R;
 import com.example.bonz.uniap_fake.dbcontext.DBContext;
 import com.example.bonz.uniap_fake.model.AccountModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnLogin;
     private EditText edtAccount, edtPassword;
+
+    //firebase
+    DatabaseReference mDatabase;
 
     //database
     private DBContext dbContext;
 
     //list DB
-    private List<AccountModel> list;
+    private List<AccountModel> listAccount;
 
 
     @Override
@@ -31,19 +42,63 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnLogin = (Button)findViewById(R.id.btn_login);
+        //init
+        init();
+        //initFirebase
+        initFirebase();
 
-        btnLogin.setOnClickListener(this);
+        writeNewAccount();
+        //Read from the database
+        readFirebase();
 
         dbContext = DBContext.getInst();
-        dbContext.addAccount(AccountModel.create(1,"bbb","1",1));
-        dbContext.addAccount(AccountModel.create(2,"aaa","bbb",2));
-        dbContext.addAccount(AccountModel.create(3,"aaa","bbb",1));
+        // dbContext.addAccount(AccountModel.create(1, "bbb", "1", 1));
+        //dbContext.addAccount(AccountModel.create(2, "aaa", "bbb", 2));
+        //dbContext.addAccount(AccountModel.create(3, "aaa", "bbb", 1));
         //dbContext.addTeacher(TeacherModel.create(1,1,"aaaa","sss","sssss","3123","SE232"));
         //dbContext.addTeacher(TeacherModel.create(333,3,"aaaa","sss","sssss","3123","SE232"));
         //dbContext.deleteAllAccount();
-        list = dbContext.getAllAccount();
-        Log.v("data", list.toString());
+        //list = dbContext.getAllAccount();
+        listAccount = new ArrayList<AccountModel>();
+        //Log.v("data", list.toString());
+
+    }
+
+    private void init() {
+        //UI
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        edtAccount = (EditText) findViewById(R.id.edt_account);
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+        //event
+        btnLogin.setOnClickListener(this);
+    }
+
+    private void initFirebase() {
+        mDatabase = FirebaseDatabase.getInstance().getReference("root");
+    }
+
+    private void writeNewAccount() {
+
+        // mDatabase.child("account").child(String.valueOf(4)).setValue(AccountModel.create(4, "bbb", "1", 1));
+    }
+
+    //read Database from Firebase
+    private void readFirebase() {
+        mDatabase.child("account").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    AccountModel model = ds.getValue(AccountModel.class);
+                    listAccount.add(model);
+                    //Log.d("test", "Value is: " + ds.getValue(AccountModel.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -51,12 +106,39 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         int id = v.getId();
         switch (id) {
             case R.id.btn_login:
-                Log.v("mess", "ok");
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
+                onClickLogin(v);
                 break;
             default:
                 break;
         }
+    }
+
+    //function onClick
+    private void onClickLogin(View v) {
+        if (!edtAccount.getText().toString().equals("") && !edtPassword.getText().toString().equals("")) {
+            if(checkLogin()){
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }else {
+                Snackbar.make(v, "Account & Password Incorrect !!!", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+            }
+        } else {
+            Snackbar.make(v, "Enter Account & Password please !!!", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null).show();
+        }
+
+
+    }
+
+    //Check login
+    private boolean checkLogin() {
+        for (AccountModel model : listAccount) {
+            if (edtAccount.getText().toString().equals(model.getUsername())
+                    && edtAccount.getText().toString().equals(model.getUsername())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
