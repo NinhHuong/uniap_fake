7b93985b7beac07e0d145374809cd42c3d5b4175
Dang Quang Si <quangsi@coloralpha.com>
2017-03-21 13:46:29 +0700

    Add sample data

--- app/src/main/java/com/example/bonz/uniap_fake/dbcontext/TimetaleDBContext.java
+++ app/src/main/java/com/example/bonz/uniap_fake/dbcontext/TimetaleDBContext.java
@@ -1,7 +1,9 @@
 package com.example.bonz.uniap_fake.dbcontext;
 
-import com.example.bonz.uniap_fake.model.AttendanceModel;
+import com.example.bonz.uniap_fake.model.LectureModel;
 import com.example.bonz.uniap_fake.model.TimetableEntity;
+
+import io.realm.RealmResults;
 
 /**
  * Created by bonz on 3/14/17.
@@ -10,7 +12,8 @@ import com.example.bonz.uniap_fake.model.TimetableEntity;
 public class TimetaleDBContext extends DBContext {
 
     public TimetableEntity getTimetableBy(String date, int num, String studentId) {
-        realm.where(AttendanceModel.class).equalTo("studentModel.id", id);
+        RealmResults<LectureModel> realmResults = realm.where(LectureModel.class)
+                .findAll();
 
         return null;
     }
